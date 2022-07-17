a36b87e5d74a9b4372dcc1a4b91b2c774f9000dc
hajee-apsis <hajee.udg@gmail.com>
2022-07-17 22:43:27 +0530

    modified 0002 to see runtime value change for variable

--- 0002-variable_and_data.js
+++ 0002-variable_and_data.js
@@ -1,5 +1,16 @@
-console.log( "Here we will going to study the variable and data assigning" )
+let title = "Variable and data assigning"
+
+console.log( title, "Here we will going to study the variable and data assigning" )
 
 let a = 10
 
-console.log( "Before Changing", a )
+console.log( `Before Changing value of "a" is a number : ${ a }` )
+
+
+a = title
+
+
+console.log( `After Changing value of "a" to "title" which is a string : ${ a }` )
+
+
+console.log( "This is called runtime value change which is a feature of JavaScript" )
