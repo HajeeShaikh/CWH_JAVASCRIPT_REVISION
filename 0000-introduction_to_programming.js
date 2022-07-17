a92951fba896155032ea27f2b63a791edb852dc9
Shaikh_Hajee <shaikh.hajee@apsissolutions.com>
2022-07-17 21:22:53 +0530

    variable and data video completed and added ext js file to html

---
+++ 0000-introduction_to_programming.js
@@ -0,0 +1,8 @@
+function addThreeNumbers ( a, b, c )
+{
+    return ( a + b + c )
+}
+
+
+let result = addThreeNumbers( 1, 2, 3 )
+console.log( result );
---
+++ 0002-variable_and_data.js
@@ -0,0 +1,5 @@
+console.log( "Here we will going to study the variable and data assigning" )
+
+let a = 10
+
+console.log( "Before Changing", a )
