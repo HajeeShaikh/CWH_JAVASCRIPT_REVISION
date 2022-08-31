0f557ce742808b93bec671c5084e6ab422a968c0
hajee-apsis <hajee.udg@gmail.com>
2022-09-01 02:23:57 +0530

    modified room_expenses_without_leaves.js

--- programs/Room_Expenses/room_expenses_without_leaves.js
+++ programs/Room_Expenses/room_expenses_without_leaves.js
@@ -1,25 +1,40 @@
-/*
+
 
 const prompt = require( "prompt-sync" )();
+
+
+// let garbage = prompt( "Enter Payment for garbage:===> " )
+// let water = prompt( "Enter Payment for water:===> " )
+
+/*for line spacing and clear vision using console.log( "\n" )
+Also it is helping in stopping in un-necessary indentation of any single digit change in terminal
+*/
+
+console.log( "\n" );
 
 let P1Contribution = prompt( "Enter contribution of Fauzan =====> : " )
 P1Contribution = Number.parseInt( P1Contribution )
 
+console.log( "\n" );
 
 let P2Contribution = prompt( "Enter contribution of Altamash =====> : " )
 P2Contribution = Number.parseInt( P2Contribution )
+console.log( "\n" );
 
 
 let P3Contribution = prompt( "Enter contribution of Shoaib =====> : " )
 P3Contribution = Number.parseInt( P3Contribution )
+console.log( "\n" );
 
 
 let P4Contribution = prompt( "Enter contribution of Rafiq =====> : " )
 P4Contribution = Number.parseInt( P4Contribution )
+console.log( "\n" );
 
 
 let P5Contribution = prompt( "Enter contribution of Hajee =====> : " )
 P5Contribution = Number.parseInt( P5Contribution )
+console.log( "\n" );
 
 
 const Total = P1Contribution +
@@ -28,36 +43,71 @@ const Total = P1Contribution +
   P4Contribution +
   P5Contribution
 
+console.log( "\n" );  // for line spacing
 
-console.log( "Total ====> : ", Total );
+console.log( " Total ====> : ", Total );
 
 const perHeadContribution = Total / 5
 
+console.log( "\n" );
+
 console.log( "perHeadContribution =====> : ", perHeadContribution );
+
+console.log( "\n" );
 
 // For checking whose contribution is more
 
-const PCount = []
+const PCountWithNames = []
 
-PCount.push( P1Contribution )
-PCount.push( P2Contribution )
-PCount.push( P3Contribution )
-PCount.push( P4Contribution )
-PCount.push( P5Contribution )
+// PCountWithNames.push( `ContributionOfFauzan = ${ P1Contribution }` )
+PCountWithNames.push( { "Fauzan": `${ P1Contribution }₹` } )
+// PCountWithNames.push( `ContributionOfAltamash =  ${ P2Contribution }` )
+PCountWithNames.push( { "Altamsh": `${ P2Contribution }₹` } )
+// PCountWithNames.push( `ContributionOfShoaib = ${ P3Contribution }` )
+PCountWithNames.push( { "Shoaib": `${ P3Contribution }₹` } )
+// PCountWithNames.push( `ContributionOfRafiq = ${ P4Contribution }` )
+PCountWithNames.push( { "Rafiq": `${ P4Contribution }₹` } )
+// PCountWithNames.push( `ContributionOfHajee = ${ P5Contribution }` )
+PCountWithNames.push( { "Hajee": `${ P5Contribution }₹` } )
 
+console.log( "\n" );
+
+console.log( "PCountWithNames Array is =====> : ", PCountWithNames );
+
+console.log( "\n" );
+
+// Regaining only number values from above array of objects
+
+let PCount = []
+for ( let objects of PCountWithNames )
+{
+  let a = ( ( Object.values( objects ) ) )[ 0 ].replace( "₹", ".0" )
+
+  PCount.push( parseInt( a ) )
+
+}
+
+// Total Re - verification from array
+
+console.log( "\n" )
+
+console.log( "Total Re-verified from array =====> : ", PCount.reduce( ( a, b ) =>
+{
+  return a + b
+} ) )
+
+console.log( "\n" )
 console.log( "PCount Array is =====> : ", PCount );
-
+console.log( "\n" )
 const bigContribution = ( Math.max( ...PCount ) )
 const smallContribution = ( Math.min( ...PCount ) )
 
 console.log( "Big Contribution is =====> : ", bigContribution )
+console.log( "\n" )
 console.log( "Small Contribution is =====> : ", smallContribution )
+console.log( "\n" )
 
-// if ( bigContribution > perHeadContribution )
-// {
-//   const amountToBeDistributed = bigContribution - perHeadContribution
-//   console.log( "amountToBeDistributed =====>  : ", amountToBeDistributed )
-// }
+
 
 // Amount to be distributed should be returned to big contributor by all P
 
@@ -72,19 +122,25 @@ let AmountShouldPayByP1 = perHeadContribution - P1Contribution
 
 if ( AmountShouldPayByP1 < 0 )
 {
+  console.log( "\n" )
   console.log( "Fauzan is biggest contributor wait for your return of investment/contribution" )
-
+  console.log( "\n" )
   console.log( "You should get your extra Amount from CollectedAmountHolder is =====> : ", ( P1Contribution - perHeadContribution ) )
+  console.log( "\n" )
 
 }
 else if ( AmountShouldPayByP1 == 0 )
 {
+  console.log( "\n" )
   console.log( "Fauzan , your contribution is equal to per head contribution" )
+  console.log( "\n" )
 }
 else
 {
   distributedAmountCollector.push( AmountShouldPayByP1 )
+  console.log( "\n" )
   console.log( "Amount Should Pay By Fauzan to CollectedAmountHolder =====> : ", AmountShouldPayByP1 )
+  console.log( "\n" )
 }
 
 // P2 Calculations
@@ -96,18 +152,24 @@ let AmountShouldPayByP2 = perHeadContribution - P2Contribution
 
 if ( AmountShouldPayByP2 < 0 )
 {
+  console.log( "\n" )
   console.log( "Altamsh is biggest contributor wait for your return of investment/contribution" )
+  console.log( "\n" )
   console.log( "Altamash should get your extra Amount from CollectedAmountHolder is =====> : ", ( P2Contribution - perHeadContribution ) )
-
+  console.log( "\n" )
 }
 else if ( AmountShouldPayByP2 == 0 )
 {
+  console.log( "\n" )
   console.log( "Altamsh , your contribution is equal to per head contribution" )
+  console.log( "\n" )
 }
 else
 {
   distributedAmountCollector.push( AmountShouldPayByP2 )
+  console.log( "\n" )
   console.log( "Amount Should Pay By Altamsh to CollectedAmountHolder =====> : ", AmountShouldPayByP2 )
+  console.log( "\n" )
 }
 
 // P3 Calculations
@@ -117,18 +179,24 @@ let AmountShouldPayByP3 = perHeadContribution - P3Contribution
 
 if ( AmountShouldPayByP3 < 0 )
 {
+  console.log( "\n" )
   console.log( "Shoaib is biggest contributor wait for your return of investment/contribution" )
+  console.log( "\n" )
   console.log( "Shoaib should get your extra Amount from CollectedAmountHolder is =====> : ", ( P3Contribution - perHeadContribution ) )
-
+  console.log( "\n" )
 }
 else if ( AmountShouldPayByP3 == 0 )
 {
+  console.log( "\n" )
   console.log( "Shoaib , your contribution is equal to per head contribution" )
+  console.log( "\n" )
 }
 else
 {
   distributedAmountCollector.push( AmountShouldPayByP3 )
+  console.log( "\n" )
   console.log( "Amount Should Pay By Shoaib to CollectedAmountHolder =====> : ", AmountShouldPayByP3 )
+  console.log( "\n" )
 }
 
 
@@ -140,18 +208,24 @@ let AmountShouldPayByP4 = perHeadContribution - P4Contribution
 
 if ( AmountShouldPayByP4 < 0 )
 {
+  console.log( "\n" )
   console.log( "Rafiq is biggest contributor wait for your return of investment/contribution" )
+  console.log( "\n" )
   console.log( "Rafiq should get your extra Amount from CollectedAmountHolder is =====> : ", ( P4Contribution - perHeadContribution ) )
-
+  console.log( "\n" )
 }
 else if ( AmountShouldPayByP4 == 0 )
 {
+  console.log( "\n" )
   console.log( "Rafiq , your contribution is equal to per head contribution" )
+  console.log( "\n" )
 }
 else
 {
   distributedAmountCollector.push( AmountShouldPayByP4 )
+  console.log( "\n" )
   console.log( "Amount Should Pay By Rafiq to CollectedAmountHolder =====> : ", AmountShouldPayByP4 )
+  console.log( "\n" )
 }
 
 // P5 Calculations
@@ -162,32 +236,40 @@ let AmountShouldPayByP5 = perHeadContribution - P5Contribution
 
 if ( AmountShouldPayByP5 < 0 )
 {
+  console.log( "\n" )
   console.log( "Hajee is biggest contributor wait for your return of investment/contribution" )
+  console.log( "\n" )
   console.log( "Hajee should get your extra Amount from CollectedAmountHolder is =====> : ", ( P5Contribution - perHeadContribution ) )
+  console.log( "\n" )
 }
 else if ( AmountShouldPayByP5 == 0 )
 {
+  console.log( "\n" )
   console.log( "Hajee , your contribution is equal to per head contribution" )
+  console.log( "\n" )
 }
 else
 {
   distributedAmountCollector.push( AmountShouldPayByP5 )
+  console.log( "\n" )
   console.log( "Amount Should Pay By Hajee to CollectedAmountHolder =====> : ", AmountShouldPayByP5 )
+  console.log( "\n" )
 }
 
-
+console.log( "\n" )
 console.log( "distributedAmountCollector =====> : ", distributedAmountCollector );
+console.log( "\n" )
+
+// Calculation of total distributed amount
 
 
-let CollectedAmountHolder = 0
-
-for ( let i = 0; i < distributedAmountCollector.length; i++ )
+let CollectedAmountHolder = distributedAmountCollector.reduce( ( a, b ) =>
 {
-  CollectedAmountHolder += distributedAmountCollector[ i ];
-
-}
+  return a + b
+}, 0 )        // Here initial value is must if all contributions are equal.
+console.log( "\n" )
 console.log( "CollectedAmountHolder now have =====> : ", CollectedAmountHolder )
-
+console.log( "\n" )
 
 
 
@@ -199,60 +281,72 @@ console.log( "CollectedAmountHolder now have =====> : ", CollectedAmountHolder )
 
 // means AmountShouldPayByP1 < 0
 
+// means he wants his investment back i.e. 
+
+// returnOfContributionOfFauzan = investmentOfFauzan - perHeadContribution
+
+
 if ( AmountShouldPayByP1 < 0 )
 {
-  const FinalAmountOfFauzan = CollectedAmountHolder - ( P1Contribution - perHeadContribution )
 
-  console.log( "FinalAmountOfFauzan =====> : ", FinalAmountOfFauzan )
+  const returnOfContributionOfFauzan = P1Contribution - perHeadContribution
+
+  CollectedAmountHolder = CollectedAmountHolder - returnOfContributionOfFauzan
+  console.log( "\n" )
+  console.log( `After paying Fauzan' s extra contribution which is = 
+   ${ returnOfContributionOfFauzan }₹; CollectedAmountHolder have ${ CollectedAmountHolder }₹` )
+  console.log( "\n" )
 }
 // if Altamash is biggest contributor
 
-else if ( AmountShouldPayByP2 < 0 )
+if ( AmountShouldPayByP2 < 0 )
 {
-  const FinalAmountOfAltamash = CollectedAmountHolder - ( P2Contribution - perHeadContribution )
-  console.log( "FinalAmountOfAltamash =====> : ", FinalAmountOfAltamash )
+  const returnOfContributionOfAltamash = P2Contribution - perHeadContribution
+
+  CollectedAmountHolder = CollectedAmountHolder - returnOfContributionOfAltamash
+  console.log( "\n" )
+  console.log( `After paying Altamash' s extra contribution which is = 
+   ${ returnOfContributionOfAltamash }₹; CollectedAmountHolder have ${ CollectedAmountHolder }₹` )
+  console.log( "\n" )
 }
 // if Shoaib is biggest contributor 
 
-else if ( AmountShouldPayByP3 < 0 )
+if ( AmountShouldPayByP3 < 0 )
 {
-  const FinalAmountOfShoaib = CollectedAmountHolder - ( P3Contribution - perHeadContribution )
-  console.log( "FinalAmountOfShoaib =====> : ", FinalAmountOfShoaib )
+  const returnOfContributionOfShoaib = P3Contribution - perHeadContribution
+
+  CollectedAmountHolder = CollectedAmountHolder - returnOfContributionOfShoaib
+  console.log( "\n" )
+  console.log( `After paying Shoaib' s extra contribution which is = 
+   ${ returnOfContributionOfShoaib }₹; CollectedAmountHolder have ${ CollectedAmountHolder }₹` )
+  console.log( "\n" )
 }
 
 // if Rafiq is biggest contributor
 
-else if ( AmountShouldPayByP4 < 0 )
+if ( AmountShouldPayByP4 < 0 )
 {
-  const FinalAmountOfRafiq = CollectedAmountHolder - ( P4Contribution - perHeadContribution )
-  console.log( "FinalAmountOfRafiq =====> : ", FinalAmountOfRafiq )
+  const returnOfContributionOfRafiq = P4Contribution - perHeadContribution
+
+  CollectedAmountHolder = CollectedAmountHolder - returnOfContributionOfRafiq
+  console.log( "\n" )
+  console.log( `After paying Rafiq' s extra contribution which is = 
+   ${ returnOfContributionOfRafiq }₹; CollectedAmountHolder have ${ CollectedAmountHolder }₹` )
+
+  console.log( "\n" )
 }
 
 
 // if Hajee is biggest contributor
 
-else if ( AmountShouldPayByP5 < 0 )
+if ( AmountShouldPayByP5 < 0 )
 {
-  const FinalAmountOfHajee = CollectedAmountHolder - ( P5Contribution - perHeadContribution )
-  console.log( "FinalAmountOfHajee =====> : ", FinalAmountOfHajee )
+  const returnOfContributionOfHajee = P5Contribution - perHeadContribution
+
+  CollectedAmountHolder = CollectedAmountHolder - returnOfContributionOfHajee
+  console.log( "\n" )
+  console.log( `After paying Hajee' s extra contribution which is = 
+   ${ returnOfContributionOfHajee }₹; CollectedAmountHolder have ${ CollectedAmountHolder }₹` )
+
+  console.log( "\n" )
 }
-
-
-
-// const CollectThisByOne = distributedAmountCollector.reduce(
-//   ( accumulator, currentValue ) =>
-//   {
-//     accumulator + currentValue
-
-//   } )
-
-// console.log( "Collect This by one =====> : ", CollectThisByOne )
-
-// if ( ( AmountShouldPayByP5 > 0 ) || ( AmountShouldPayByP4 > 0 ) || ( AmountShouldPayByP3 > 0 ) || ( AmountShouldPayByP2 > 0 ) || ( AmountShouldPayByP1 > 0 ) )
-// {
-//   const TotalAmountShouldBePaid = AmountShouldPayByP5 + AmountShouldPayByP4 + AmountShouldPayByP3 + AmountShouldPayByP2 + AmountShouldPayByP1
-
-//   console.log( "TotalAmountShouldBePaid =====> : ", TotalAmountShouldBePaid )
-// }
-
-*/
