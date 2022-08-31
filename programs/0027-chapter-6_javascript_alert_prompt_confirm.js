899e66da142440a1d664b5b25d621e0c26f7094d
hajee-apsis <hajee.udg@gmail.com>
2022-08-31 15:59:38 +0530

    Added DOM, BOM & Window object

---
+++ programs/0027-chapter-6_javascript_alert_prompt_confirm.js
@@ -0,0 +1,15 @@
+/*
+alert( "Hi I am alert...😅  ❗" )
+
+let a = prompt( "Enter A Number", 90 )    // second argument is default value
+
+let consoleData = confirm( "Do you want to console a..?" )
+
+if ( consoleData )
+{
+  console.log( "Value of a :===> ", a );
+} else
+{
+  console.warn( "Allow me to show value of a" );
+}
+*/
