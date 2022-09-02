f434659ee656f378af3d1b6762d9da2f1f3f60c9
hajee-apsis <hajee.udg@gmail.com>
2022-09-02 18:50:05 +0530

    Added some styles with css and javascript

--- programs/0028-chapter-6_dom_bom_window_object.js
+++ programs/0028-chapter-6_dom_bom_window_object.js
@@ -1,4 +1,13 @@
+/* 
+window.console.log( window );
+window.console.log( document )
+window.console.log( document.body );
+window.document.body.style.backgroundColor = "green"
+
+// As window is an global object we can write as 
+
 console.log( window );
 console.log( document )
 console.log( document.body );
 document.body.style.backgroundColor = "green"
+*/
---
+++ programs/0029-chapter-6_practice_set.js
@@ -0,0 +1,167 @@
+// const prompt = require( "prompt-sync" )()
+
+/*  Q1) Write a program using prompt function to take input of age as a value from the user and use   an alert to tell him if he can drive....
+*/
+/*
+// Ans ===>
+
+let age = prompt( "Enter your age =====> : " )
+
+age = Number.parseInt( age )
+
+// alert( age > 18 ? "You can drive...!" : "You can't drive...!" )
+
+age > 18 ? alert( "You can drive...!" ) : alert( "You can't drive...!" )
+
+*/
+
+/*
+
+// Q2) In question 1 use confirm to ask the user if he wants to see the prompt again
+
+// Ans ===>
+
+
+
+while ( true )
+{
+
+  if ( confirm( "Do you want to show prompt again...?" ) )
+  {
+
+    let age = prompt( "Enter your age =====> : " )
+
+    age = Number.parseInt( age )
+
+    age > 18 ? alert( "You can drive...!" ) : alert( "You can't drive...!" )
+  }
+  else
+  {
+    break
+  }
+
+
+}
+
+*/
+
+// Q3) In the previous question, use console.error to log the error if the age entered is negative.
+
+// Ans ===>
+
+/*
+
+while ( true )
+{
+  if ( confirm( "Do you want to show prompt again...?" ) )
+  {
+    let age = prompt( "Enter your age =====> : " )
+
+    age = Number.parseInt( age )
+
+    if ( age < 0 )
+    {
+      console.error( "You have entered an invalid age.....😅" )
+    }
+    else
+    {
+
+      age > 18 ? alert( "You can drive...!" ) : alert( "You can't drive...!" )
+    }
+  }
+  else
+  {
+    break
+  }
+}
+
+*/
+
+/*
+Q4) Write a program to change the url to google.com (redirection) if user enters a
+number greater than 4.
+*/
+
+/*
+
+// Ans ===>
+
+
+
+while ( true )
+{
+  if ( confirm( "Do you want to show prompt again...?" ) )
+  {
+    let inputNumber = prompt( "Enter A Number =====> : " )
+
+    inputNumber = Number.parseInt( inputNumber )
+
+    if ( inputNumber > 4 )
+    {
+      document.location.href = "https://www.google.com"
+    }
+    else
+    {
+
+      inputNumber < 4 ? alert( "You can cancel...!" ) : console.log( "Number is less than 4" );
+    }
+  }
+  else
+  {
+    break
+  }
+}
+
+
+// OR
+
+
+
+location.href = "https://www.google.com"
+
+*/
+// OR
+
+// let inputNumber = prompt( "Enter A Number : ===> " )
+
+// inputNumber > 4 ? document.location.href = "https://www.google.com" : ""
+
+
+// Q5) Change the background of the page to yellow or red or any other color based on the user input through prompt.
+
+// let a = setInterval( () =>
+// {
+
+//   let backgroundColor = prompt( "Your expectation of background color  ===> : " )
+//   backgroundColor ? document.body.style.backgroundColor = backgroundColor : ""
+// }, 10000 );
+
+// clearInterval( a )
+
+// M.S solution
+
+// keep changing background color continously
+
+/*
+  setInterval( () =>
+            {
+
+                randomColor = "#" + Math.floor( Math.random() * 16777215 ).toString( 16 );
+
+            }, 1000 );
+
+            console.log( randomColor, "random Color" );
+
+
+
+            setInterval( () =>
+            {
+
+                document.body.style.backgroundColor = randomColor
+            }, 1000 );  
+*/
+
+
+
+
+
